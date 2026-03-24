a36f035d1fe0445cbef5e66204abc32dba3d2977
Simon Gavelin <simon.gavelin06@gmail.com>
2026-03-24 21:49:40 +0100

    Initial commit

---
+++ src/main/java/Graphics/Main.java
@@ -0,0 +1,15 @@
+package Graphics;
+
+import javafx.application.Application;
+
+public class Main extends Application {
+    @Override
+    public void start(javafx.stage.Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
